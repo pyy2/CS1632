3a6cd99e1a6cbc3cbf878d497db4923cd57af08d
Paul Yu <pyy2@pitt.edu>
2018-10-31 07:16:51 -0400

    args test done

--- verify_args.rb
+++ verify_args.rb
@@ -4,7 +4,7 @@ require 'minitest/autorun'
 # program doesn't start when running MiniTest
 class VerifyArgs
   def verify_args(args)
-    args.count == 2 && args[0] = Integer(args[0]) && args[1].to_i > 0 && args[1] = Integer(args[1])
+    args.count == 2 && args[0] = Integer(args[0]) && args[1].to_i > 0
   rescue StandardError
     false
   end
--- verify_args_test.rb
+++ verify_args_test.rb
@@ -2,4 +2,56 @@ require 'minitest/autorun'
 require_relative 'verify_args.rb'
 
 class ValidArgsTest < MiniTest::Test
+
+    # Special method that is run before each test to reuse @v instance
+    # variable such that we don't have to type m = GoldRush::new in each test
+    def setup
+        @v = VerifyArgs.new
+    end
+
+    # UNIT TESTS FOR METHOD verify_args(args)
+    # Equivalence classes:
+    # @v = [-INFINITY..-1,-INFINITY..-1] -> returns false
+    # @v = [-INFINITY..INFINITY,1..INFINITY] -> returns true
+    # @v = (not a number) -> returns false
+  
+    # If there are no command line arguments then
+    # false is returned
+    def test_verify_args_no_args
+      assert_equal false, @v.verify_args([])
+    end
+  
+    # If there is only 1 command line argument, then
+    # false is returned
+    def test_verify_args_one_arg
+      assert_equal false, @v.verify_args([1])
+      assert_equal false, @v.verify_args(['hi'])
+    end
+  
+    # If there are 2 string arguments, then
+    # false is returned
+    # EDGE CASE
+    def test_verify_args_two_string_args
+      assert_equal false, @v.verify_args(['a', 'b'])
+    end
+  
+    # If there is 1 string and 1 integer argument, then
+    # false is returned
+    # EDGE CASE
+    def test_verify_args_one_string_one_int
+      assert_equal false, @v.verify_args(['a', 1])
+      assert_equal false, @v.verify_args([1, 'b'])
+    end
+  
+    # If the second argument is a negative integer, then
+    # false is returned
+    def test_verify_args_negative_second_args
+      assert_equal false, @v.verify_args([0, -2])
+    end
+
+    # If there are 2 integer arguments with the second
+    # argument as a non-negative integer, then true is returned
+    def test_verify_args_valid
+      assert_equal true, @v.verify_args([1, 10])
+    end
 end
