5a2bad632d8e2b4d8db9d6d52693db0587f9442f
Paul Yu <pyy2@pitt.edu>
2018-10-31 08:37:33 -0400

    randseedtest

--- gold_rush_test.rb
+++ gold_rush_test.rb
@@ -1,5 +1,12 @@
 require 'minitest/autorun'
+require_relative 'gold_rush.rb'
 
 class GoldRushTest < Minitest::Test
+    # Special method that is run before each test to reuse @v instance
+    # variable such that we don't have to type m = GoldRush::new in each test
+    def setup
+        @g = GoldRush.new(args[1, 1])
+    end
+
 
 end
---
+++ seed_rand_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require_relative 'seed_rand.rb'
+
+class SeedRandTest < MiniTest::Test
+
+    # UNIT TEST FOR METHOD srand(args)
+    # Equivalence classes:
+    # @v = [-INFINITY..INFINITY..] -> returns true
+  
+    # verifies that seeding the random number generator
+    # it is not nil.
+    def test_rand_seed_generator
+        seed = Seed.new 1
+        refute_nil seed
+    end
+end
